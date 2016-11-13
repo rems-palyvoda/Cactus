85c88bd8bc61ae4abb24a5c3848b00d1740c4a86
rems-palyvoda <rems.palyvoda@gmail.com>
2016-11-13 13:22:52 +0200

    fix test user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -38,5 +40,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :avatar,
                                   :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
     end
 end
