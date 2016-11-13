310e7bb3ed14b5146147d2f32794796330351c8f
rems-palyvoda <rems.palyvoda@gmail.com>
2016-11-13 15:13:17 +0200

    checking current/correct user in session

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,36 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:one)
+    @other_user = users(:two)
+  end
+
   test "should get new" do
     get "/users/new"
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as other user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when looged in as other user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { first_name: @user.first_name,
+                                              last_name: @user.last_name,
+                                              email: @user.email } } 
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
